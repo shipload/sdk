5fb34492604f74cc2eb5d98b9e4c25466ca93676
Aaron Cox <aaron@greymass.com>
2024-05-28 15:37:48 -0700

    Fixed epoch calculation

--- src/epoch.ts
+++ src/epoch.ts
@@ -1,11 +1,8 @@
-import {PlatformContract, ServerContract} from './contracts'
+import {PlatformContract} from './contracts'
 
-export async function getCurrentEpoch(
-    game: PlatformContract.Types.game_row,
-    state: ServerContract.Types.state_row
-): Promise<number> {
+export async function getCurrentEpoch(game: PlatformContract.Types.game_row): Promise<number> {
     const current = new Date().getTime()
-    const difference = (current - state.genesis.toDate().getTime()) / 1000
+    const difference = (current - game.config.start.toDate().getTime()) / 1000
     const epoch = Math.floor(difference / Number(game.config.epochtime)) + 1
     return epoch
 }
